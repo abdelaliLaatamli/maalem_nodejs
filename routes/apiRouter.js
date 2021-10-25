542b47b2bd8b60b5d6898d03252bb9f97011899b
Abdelali Laatamli <alaatamli@presenceinteractive.com>
2021-10-25 18:44:54 +0100

    add user controller

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,10 +1,18 @@
 const express = require('express') ;
+const userController = require('./../controllers/userController');
 
 
 // routes 
 exports.router = (()=>{
 
     var apiRouter = express.Router();
+
+    // user routes
+    // apiRouter.route('/user/register/').post( userAPI.register );
+    // apiRouter.route('/user/login/').post( userAPI.login );
+    // apiRouter.route('/user/profile').get( userAPI.getUserProfile );
+    // apiRouter.route('/user').get( userAPI.getuserDetails );
+    // apiRouter.route('/user').put( userAPI.getUserUpdate );
 
     return apiRouter;
 
